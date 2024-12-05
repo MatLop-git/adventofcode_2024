f992c4f6e95782935d6b840ffc3ee7a6392e39e7
matlopez <mat.hack@gmail.com>
2024-12-05 14:58:28 -0300

    Day03

--- Day03/day03.cpp
+++ Day03/day03.cpp
@@ -22,60 +22,69 @@ public:
         return std::regex_match(s, number_regex);
     }
 
-    std::pair<int, int> parseMulValue(std::string str, int beginPos, int endPos)
+    std::pair<int, int> parseMulValue(std::string str, int &beginPos, int &endPos)
     {
-        std::pair<int, int> mulValue;
+        std::pair<int, int> mulValue(-1, -1);
 
+        // find "mul("
+        bool found = false;
+        do
+        {
+            beginPos = str.find("mul(", beginPos);
+            if( beginPos > -1 )
+            {
+                // discard "mul(" from search
+                beginPos += 4;
+                // find ','
+                endPos = str.find_first_of(',', beginPos);
+                if( endPos > -1 )
+                {
+                    // validate is number
+                    std::string value = str.substr(beginPos, endPos-beginPos);
+                    if( isNumber(value) )
+                    {
+                        mulValue.first = std::stoi( value );
+                        beginPos = endPos + 1;
+                        // find ')'
+                        endPos = str.find_first_of(')', beginPos);
+                        if( endPos > -1 )
+                        {
+                            // validate is number
+                            value = str.substr(beginPos, endPos-beginPos);
+                            if( isNumber(value) )
+                            {
+                                // mul value is correct, add to list and continue searching from the end of this mul(x,y)
+                                mulValue.second = std::stoi( value );
+                                beginPos = endPos + 1;
+                                found = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        while(beginPos > -1 && endPos > -1 && found == false);
 
         return mulValue;
     }
 
     void parseMulValues()
     {
-        // parse values and put these into aux arrays
+        _mulValues.clear();
+        // parse values and put these into aux vector
         std::string currentLine;
         for(int i=0; i<_fileInput.size(); ++i)
         {
-            // get mul values
             std::string currentLine = _fileInput[i];
             auto beginPos = 0;
             auto endPos = 0;
-            int value = 0;
+            // get mul values
             do
             {
-                // find "mul("
-                beginPos = currentLine.find("mul(", beginPos);
-                if( beginPos > -1 )
+                std::pair mulValue = parseMulValue(currentLine, beginPos, endPos);
+                if( mulValue.first != -1 )
                 {
-                    std::pair<int, int> mulValue;
-                    // discard "mul(" from search
-                    beginPos += 4;
-                    // find ','
-                    endPos = currentLine.find_first_of(',', beginPos);
-                    if( endPos > -1 )
-                    {
-                        // validate is number
-                        std::string value = currentLine.substr(beginPos, endPos-beginPos);
-                        if( isNumber(value) )
-                        {
-                            mulValue.first = std::stoi( value );
-                            beginPos = endPos + 1;
-                            // find ')'
-                            endPos = currentLine.find_first_of(')', beginPos);
-                            if( endPos > -1 )
-                            {
-                                // validate is number
-                                value = currentLine.substr(beginPos, endPos-beginPos);
-                                if( isNumber(value) )
-                                {
-                                    // mul value is correct, add to list and continue searching from the end of this mul(x,y)
-                                    mulValue.second = std::stoi( value );
-                                    _mulValues.push_back( mulValue );
-                                    beginPos = endPos + 1;
-                                }
-                            }
-                        }
-                    }
+                    _mulValues.push_back( mulValue );
                 }
             }
             while(beginPos > -1 && endPos > -1);
@@ -84,6 +93,72 @@ public:
 
     void parseMulValuesWithConditionals()
     {
+        _mulValues.clear();
+        // parse values and put these into aux vector
+        std::string currentLine;
+        bool enabled = true;
+        for(int i=0; i<_fileInput.size(); ++i)
+        {
+            std::string currentLine = _fileInput[i];
+            auto beginPos = 0;
+            auto endPos = 0;
+            auto nextDont = 0;
+
+            // if the last instruction on previous line was to enable we search for next "don't()"            
+            if( enabled == true )
+            {
+                nextDont = currentLine.find("don't()");
+            }
+            // but if the last instruction on previous line was to disable we search for "do()" before searching for "don't"
+            else
+            {
+                // find next "do()" and "don't()"
+                beginPos = currentLine.find("do()");
+                if( beginPos > -1 )
+                {
+                    enabled = true;
+                    // discard "do()" from search
+                    beginPos += 4;
+                    nextDont = currentLine.find("don't()", beginPos);
+                    if( nextDont < 0 )
+                    {
+                        nextDont = currentLine.size();
+                    }
+                }
+            }
+
+            // get mul values
+            do
+            {
+                std::pair mulValue = parseMulValue(currentLine, beginPos, endPos);
+                if( mulValue.first != -1 )
+                {
+                    if( endPos <= nextDont )
+                    {
+                        // enabled mul value, add to vector
+                        _mulValues.push_back( mulValue );
+                    }
+                    else
+                    {
+                        enabled = false;
+                        // disabled mul value, find next "do()" and "don't()"
+                        beginPos = currentLine.find("do()", nextDont);
+                        if( beginPos > -1 )
+                        {
+                            enabled = true;
+                            // discard "do()" from search
+                            beginPos += 4;
+                            nextDont = currentLine.find("don't()", beginPos);
+                            if( nextDont < 0 )
+                            {
+                                nextDont = currentLine.size();
+                            }
+                        }
+                    }
+                }
+            }
+            while(beginPos > -1 && endPos > -1);
+        }
     }
 
     void calculateFirstPuzzleAnswer()
@@ -95,7 +170,7 @@ public:
         for(int i=0; i<_mulValues.size(); ++i)
         {
             this->_firstPuzzleAnswer += (_mulValues[i].first * _mulValues[i].second);
-            std::cout << "mul(" << _mulValues[i].first << "," << _mulValues[i].second << ")" << std::endl;
+//            std::cout << "mul(" << _mulValues[i].first << "," << _mulValues[i].second << ")" << std::endl;
         }
     }
 
@@ -108,7 +183,7 @@ public:
         for(int i=0; i<_mulValues.size(); ++i)
         {
             this->_secondPuzzleAnswer += (_mulValues[i].first * _mulValues[i].second);
-            std::cout << "mul(" << _mulValues[i].first << "," << _mulValues[i].second << ")" << std::endl;
+//            std::cout << "mul(" << _mulValues[i].first << "," << _mulValues[i].second << ")" << std::endl;
         }
     }
 
