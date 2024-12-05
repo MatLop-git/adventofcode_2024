f7a58977a717f0aa2c3924c082328dcb2120e33d
matlopez <mat.hack@gmail.com>
2024-12-05 11:17:25 -0300

    Days 01 and 02

---
+++ Day01/day01.cpp
@@ -0,0 +1,119 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+class Helper
+{
+private:
+    static const int MAX_VALUE = 100000;
+
+    int _leftListArray[MAX_VALUE] = {0};
+    int _rightListArray[MAX_VALUE] = {0};
+
+public:
+    int _firstPuzzleAnswer = 0;
+    int _secondPuzzleAnswer = 0;
+    std::vector<std::string> _fileInput;
+
+    std::vector<int> _leftList;
+    std::vector<int> _rightList;
+
+    void parseValues()
+    {
+        // parse values and put these into aux arrays
+        std::string currentLine;
+        for(int i=0; i<_fileInput.size(); ++i)
+        {
+            // get values
+            std::string currentLine = _fileInput[i];
+            auto pos = currentLine.find_first_of(' ');
+            int value = std::stoi( currentLine.substr(0, pos) );
+            _leftListArray[value]++;
+            pos = currentLine.find_first_not_of(' ', pos);
+            value = std::stoi( currentLine.substr(pos) );
+            _rightListArray[value]++;
+        }
+
+        // go through the aux arrays and fill the ordered vectors
+        for (int i = 0; i < MAX_VALUE; ++i)
+        {
+            for(int j=0; j<_leftListArray[i]; ++j)
+            {
+                _leftList.push_back(i);
+            }
+            for(int j=0; j<_rightListArray[i]; ++j)
+            {
+                _rightList.push_back(i);
+            }
+        }
+
+//        std::cout << _leftList.size() << " _ " << _rightList.size() << std::endl;
+//        for (int i = 0; i < _leftList.size(); ++i)
+//        {
+//            std::cout << _leftList[i] << " " << _rightList[i] << std::endl;
+//        }
+    }
+
+    void calculateFirstPuzzleAnswer()
+    {
+        this->_firstPuzzleAnswer = 0;
+
+        for (int i = 0; i < _leftList.size(); ++i)
+        {
+            this->_firstPuzzleAnswer += abs(_rightList[i] - _leftList[i]);
+        }
+    }
+
+    void calculateSecondPuzzleAnswer()
+    {
+        this->_secondPuzzleAnswer = 0;
+
+        for (int i = 0; i < _leftList.size(); ++i)
+        {
+            int value = _leftList[ i ];
+            this->_secondPuzzleAnswer += (value * _leftListArray[value] * _rightListArray[value]);
+        }
+    }
+
+   void calculateAnswers(std::string inputFileName)
+   {
+        this->readFileInput(inputFileName);
+        this->parseValues();
+        this->calculateFirstPuzzleAnswer();
+        this->calculateSecondPuzzleAnswer();
+   }
+
+   void readFileInput(std::string inputFileName)
+   {
+        std::string currentLine;
+        std::ifstream inputFile(inputFileName);
+        if ( inputFile.is_open() )
+        {
+            while ( std::getline(inputFile, currentLine) )
+            {
+                _fileInput.push_back(currentLine);
+            }
+        }
+   }
+
+   int getFirstPuzzleAnswer() { return _firstPuzzleAnswer; }
+   int getSecondPuzzleAnswer() { return _secondPuzzleAnswer; }
+};
+
+
+int main()
+{
+    std::cout << "Advent of Code 2024 - Day 01" << std::endl;
+
+    const std::string inputFileName = "input";
+    Helper helper;
+    helper.calculateAnswers(inputFileName);
+
+    int answer = helper.getFirstPuzzleAnswer();
+    std::cout << "First half answer: " << answer << std::endl;
+
+    answer = helper.getSecondPuzzleAnswer();
+    std::cout << "Second half answer: " << answer << std::endl;
+}
---
+++ Day02/day02.cpp
@@ -0,0 +1,183 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+class Helper
+{
+public:
+    int _firstPuzzleAnswer = 0;
+    int _secondPuzzleAnswer = 0;
+    std::vector<std::string> _fileInput;
+
+    std::vector<std::vector<int>> _reports;
+
+    void parseValues()
+    {
+        // parse values and put these into aux arrays
+        std::string currentLine;
+        for(int i=0; i<_fileInput.size(); ++i)
+        {
+            // get levels
+            std::vector<int> report;
+            std::string currentLine = _fileInput[i];
+            auto beginPos = 0;
+            auto endPos = 0;
+            int level = 0;
+            do
+            {
+                endPos = currentLine.find_first_of(' ', beginPos);
+                level = std::stoi( currentLine.substr(beginPos, endPos) );
+                report.push_back( level );
+                beginPos = endPos + 1;
+            }
+            while(endPos > -1);
+            _reports.push_back( report );
+        }
+    }
+
+    void calculateFirstPuzzleAnswer()
+    {
+        this->_firstPuzzleAnswer = 0;
+
+        for(int i=0; i<_reports.size(); ++i)
+        {
+            std::vector<int> report = _reports[i];
+            // calculate report direction
+            bool direction = (report[0] < report[1]);
+            // verify levels safety
+            bool safe = true;
+            int j=1;
+            do
+            {
+                int diff = report[j]-report[j-1];
+                if( direction )
+                {
+                    safe = (1 <= diff) && (diff <= 3);
+                }
+                else
+                {
+                    safe = (1 <= -diff) && (-diff <= 3);
+                }
+                j++;
+            }
+            while(safe && j<report.size());
+
+            if( safe )
+            {
+                this->_firstPuzzleAnswer++;
+            }
+        }
+    }
+
+    void calculateSecondPuzzleAnswer()
+    {
+        this->_secondPuzzleAnswer = 0;
+
+        for(int i=0; i<_reports.size(); ++i)
+        {
+            std::vector<int> report = _reports[i];
+            // calculate report direction
+            int directionAux = 0;
+            for(int j=1; j<report.size(); ++j)
+            {
+                directionAux += (report[j] > report[j-1] ? 1 : -1);
+            }
+            bool direction = directionAux > 0;
+            // verify levels safety
+            bool safe = true;
+            bool removedSingleBadLevel = false;
+            bool handleSingleBadLevel = false;
+            int j=1;
+            do
+            {
+                int diff = (handleSingleBadLevel == false ? report[j]-report[j-1] : report[j]-report[j-2]);
+                if( direction )
+                {
+                    safe = (1 <= diff) && (diff <= 3);
+                }
+                else
+                {
+                    safe = (1 <= -diff) && (-diff <= 3);
+                }
+
+                // handle remove first level
+                if( safe == false && handleSingleBadLevel == true && j == 2 )
+                {
+                    int diff = report[j]-report[j-1];
+                    if( direction )
+                    {
+                        safe = (1 <= diff) && (diff <= 3);
+                    }
+                    else
+                    {
+                        safe = (1 <= -diff) && (-diff <= 3);
+                    }
+                }
+                handleSingleBadLevel = false;
+
+                // verify if single bad level should be removed
+                if( safe == false && removedSingleBadLevel == false )
+                {
+                    safe = true;
+                    handleSingleBadLevel = true;
+                    removedSingleBadLevel = true;
+                }
+
+                j++;
+            }
+            while(safe && j<report.size());
+
+            if( safe )
+            {
+                std::cout << i+1 << ": safe" << std::endl;
+                this->_secondPuzzleAnswer++;
+            }
+            else
+            {
+                std::cout << i+1 << ": bad" << std::endl;
+            }
+        }
+    }
+
+   void calculateAnswers(std::string inputFileName)
+   {
+        this->readFileInput(inputFileName);
+        this->parseValues();
+        this->calculateFirstPuzzleAnswer();
+        this->calculateSecondPuzzleAnswer();
+   }
+
+   void readFileInput(std::string inputFileName)
+   {
+        std::string currentLine;
+        std::ifstream inputFile(inputFileName);
+        if ( inputFile.is_open() )
+        {
+            while ( std::getline(inputFile, currentLine) )
+            {
+                _fileInput.push_back(currentLine);
+            }
+        }
+   }
+
+   int getFirstPuzzleAnswer() { return _firstPuzzleAnswer; }
+   int getSecondPuzzleAnswer() { return _secondPuzzleAnswer; }
+};
+
+
+int main()
+{
+    std::cout << "Advent of Code 2024 - Day 02" << std::endl;
+
+    const std::string inputFileName = "input";
+    Helper helper;
+    helper.calculateAnswers(inputFileName);
+
+    int answer = helper.getFirstPuzzleAnswer();
+    std::cout << "First half answer: " << answer << std::endl;
+
+    answer = helper.getSecondPuzzleAnswer();
+    std::cout << "Second half answer: " << answer << std::endl;
+}
