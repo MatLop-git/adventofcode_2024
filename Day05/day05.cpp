d02d1a276f324083ddc60502bbdb38e8ecafaee3
Matias Lopez <matias.lopez@rocketinsights.com>
2024-12-06 12:53:23 -0300

    Day 05 - First half

--- Day05/day05.cpp
+++ Day05/day05.cpp
@@ -20,7 +20,7 @@ public:
     };
 
     std::map<int, OrderingRule> _orderingRules;
-    std::vector<std::vector<int>> _pagesUpdates;
+    std::vector< std::vector<int> > _pagesUpdates;
 
     void parseValues()
     {
@@ -28,7 +28,7 @@ public:
         for(int i=0; i<_fileInput.size(); ++i)
         {
             std::string currentLine = _fileInput[i];
-            if( currentLine.size() < 1 )
+            if( currentLine.size() < 3 )
             {
                 parsingRules = false;
             }
@@ -76,6 +76,45 @@ public:
         this->_firstPuzzleAnswer = 0;
 
         parseValues();
+
+        for(int i=0; i<_pagesUpdates.size(); ++i)
+        {
+            // check correct update
+            bool isCorrect = true;
+            std::vector<int> pageUpdate = _pagesUpdates[i];
+            int pageUpdateSize = pageUpdate.size();
+            int j=0;
+            while( isCorrect && (j<pageUpdateSize) )
+            {
+                int page = _pagesUpdates[i][j];
+                if( _orderingRules.find(page) != _orderingRules.end() )
+                {
+                    OrderingRule rule = _orderingRules[page];
+                    // check previous pages
+                    int k=j-1;
+                    while( isCorrect && (k>=0) )
+                    {
+                        isCorrect = (std::find(rule.pagesAfter.begin(), rule.pagesAfter.end(), pageUpdate[k]) == rule.pagesAfter.end());
+                        k--;
+                    }
+                    // check next pages
+                    k=j+1;
+                    while( isCorrect && (k<pageUpdateSize) )
+                    {
+                        isCorrect = (std::find(rule.pagesBefore.begin(), rule.pagesBefore.end(), pageUpdate[k]) == rule.pagesBefore.end());
+                        k++;
+                    }
+                }
+                j++;
+            }
+            if( isCorrect )
+            {
+                int middleValue = (pageUpdateSize/2);
+                middleValue = pageUpdate[middleValue];
+                this->_firstPuzzleAnswer += middleValue;
+//                std::cout << (i+1) << " - " << middleValue << std::endl;
+            }
+        }
     }
 
     void calculateSecondPuzzleAnswer()
