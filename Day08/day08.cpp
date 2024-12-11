642895d620eef1c61c2cb48e0c07f38c1eda0ef9
matlopez <mat.hack@gmail.com>
2024-12-10 23:28:01 -0300

    Day 08 - Second half

--- Day08/day08.cpp
+++ Day08/day08.cpp
@@ -4,12 +4,77 @@ class Helper : public IAoCHelper
 {
     int _inputWidth;
     int _inputHeight;
-    std::vector< std::vector<int> > _nodesMap;
+    std::vector< std::vector<int> > _nodesMap;  // This should be a matrix of booleans, making it a matrix of int in an attempt to anticipate to second half problem :P
 
     void prepareInput()
     {
         _inputHeight = _fileInput.size();
         _inputWidth = _fileInput[0].size();
+    }
+
+    void addAntiNodes(std::pair<int, int> antennaPos1, std::pair<int, int> antennaPos2)
+    {
+        // calculate distance
+        int distanceX = antennaPos2.first - antennaPos1.first;
+        int distanceY = antennaPos2.second - antennaPos1.second;
+
+        // add node at top
+        std::pair<int, int> nodePos;
+        nodePos.first = antennaPos1.first - distanceX;
+        nodePos.second = antennaPos1.second - distanceY;
+        if( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second >= 0) )
+        {
+            _nodesMap[nodePos.second][nodePos.first]++;
+        }
+
+        // add node at bottom
+        nodePos.first = antennaPos2.first + distanceX;
+        nodePos.second = antennaPos2.second + distanceY;
+        if( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second < _inputHeight) )
+        {
+            _nodesMap[nodePos.second][nodePos.first]++;
+        }
+    }
+
+    void addAntiNodesWithResonance(std::pair<int, int> antennaPos1, std::pair<int, int> antennaPos2)
+    {
+        // antennas are antinodes
+        _nodesMap[antennaPos1.second][antennaPos1.first]++;
+        _nodesMap[antennaPos2.second][antennaPos2.first]++;
+
+        // calculate distance
+        int distanceX = antennaPos2.first - antennaPos1.first;
+        int distanceY = antennaPos2.second - antennaPos1.second;
+
+        // add nodes at top
+        std::pair<int, int> nodePos = antennaPos1;
+        do
+        {
+            nodePos.first = nodePos.first - distanceX;
+            nodePos.second = nodePos.second - distanceY;
+            if( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second >= 0) )
+            {
+                _nodesMap[nodePos.second][nodePos.first]++;
+            }
+        }
+        while( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second >= 0) );
+
+        // add nodes at bottom
+        nodePos = antennaPos2;
+        do
+        {
+            nodePos.first = nodePos.first + distanceX;
+            nodePos.second = nodePos.second + distanceY;
+            if( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second < _inputHeight) )
+            {
+                _nodesMap[nodePos.second][nodePos.first]++;
+            }
+        }
+        while( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second < _inputHeight) );
+    }
+
+    void findAntiNodes(bool withResonance)
+    {
         _nodesMap = std::vector< std::vector<int> >(_inputHeight, std::vector<int>(_inputWidth, 0));
 
         // Look for antennas from left to right and top to bottom of file input
@@ -30,7 +95,14 @@ class Helper : public IAoCHelper
                         antennaPos2 = _fileInput[y].find_first_of(frequency, antennaPos2+1);
                         if( antennaPos2 != std::string::npos )
                         {
-                            addAntiNodes({antennaPos, y}, {antennaPos2, y});
+                            if( withResonance == true)
+                            {
+                                addAntiNodesWithResonance({antennaPos, y}, {antennaPos2, y});
+                            }
+                            else
+                            {
+                                addAntiNodes({antennaPos, y}, {antennaPos2, y});
+                            }
                         }
                     }
                     while( antennaPos2 != std::string::npos );
@@ -44,7 +116,14 @@ class Helper : public IAoCHelper
                             antennaPos2 = _fileInput[y2].find_first_of(frequency, antennaPos2+1);
                             if( antennaPos2 != std::string::npos )
                             {
-                                addAntiNodes({antennaPos, y}, {antennaPos2, y2});
+                                if( withResonance == true)
+                                {
+                                    addAntiNodesWithResonance({antennaPos, y}, {antennaPos2, y2});
+                                }
+                                else
+                                {
+                                    addAntiNodes({antennaPos, y}, {antennaPos2, y2});
+                                }
                             }
                         }
                         while( antennaPos2 != std::string::npos );
@@ -52,30 +131,6 @@ class Helper : public IAoCHelper
                 }
             }
             while( antennaPos != std::string::npos );
-        }
-    }
-
-    void addAntiNodes(std::pair<int, int> antennaPos1, std::pair<int, int> antennaPos2)
-    {
-        // calculate distance
-        int distanceX = antennaPos2.first - antennaPos1.first;      // 10 - 15 = -5     {20, top} {5, bottom}
-        int distanceY = antennaPos2.second - antennaPos1.second;    // 15 - 10 = 5      {20, bottom} { 5, top}
-
-        // add node at top
-        std::pair<int, int> nodePos;
-        nodePos.first = antennaPos1.first - distanceX;
-        nodePos.second = antennaPos1.second - distanceY;
-        if( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second >= 0) )
-        {
-            _nodesMap[nodePos.second][nodePos.first]++;
-        }
-
-        // add node at bottom
-        nodePos.first = antennaPos2.first + distanceX;
-        nodePos.second = antennaPos2.second + distanceY;
-        if( (nodePos.first >= 0) && (nodePos.first < _inputWidth) && (nodePos.second < _inputHeight) )
-        {
-            _nodesMap[nodePos.second][nodePos.first]++;
         }
     }
 
@@ -102,6 +157,9 @@ class Helper : public IAoCHelper
     {
         this->_firstPuzzleAnswer = 0;
 
+        findAntiNodes( false );
+
+        // calculate number of antinodes
         for(int i=0; i<_nodesMap.size(); ++i)
         {
             for(int j=0; j<_nodesMap[i].size(); ++j)
@@ -118,7 +176,19 @@ class Helper : public IAoCHelper
     {
         this->_secondPuzzleAnswer = 0;
 
-        // TODO
+        findAntiNodes( true );
+
+        // calculate number of antinodes
+        for(int i=0; i<_nodesMap.size(); ++i)
+        {
+            for(int j=0; j<_nodesMap[i].size(); ++j)
+            {
+                if( _nodesMap[i][j] > 0 )
+                {
+                    _secondPuzzleAnswer++;
+                }
+            }
+        }
     }
 };
 
