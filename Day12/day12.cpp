8eb0d661a8acfb8e0ec9ba06f1b151821c2b3989
matlopez <mat.hack@gmail.com>
2024-12-12 18:13:48 -0300

    Day 12 - Second half

--- Day12/day12.cpp
+++ Day12/day12.cpp
@@ -9,59 +9,76 @@ private:
     int _maxX = 0;
     int _minY = 100000;
     int _maxY = 0;
-    int _deltaX = 0;
-    int _deltaY = 0;
     std::vector< std::vector<bool> > _regionMap;
 
 public:
     char _plantType;
     int _perimeter;
+    int _sides;
     std::vector< std::pair<int, int> > _nodes;
 
     int getArea() { return _nodes.size(); }
     int getPerimeter() { return _perimeter; }
+    int getSides() { return _sides; }
     int getPrice() { return getArea() * getPerimeter(); }
+    int getPriceWithDiscount() { return getArea() * getSides(); }
+
     void setNodes(std::vector< std::pair<int, int> > nodes)
     {
         _nodes = nodes;
+        recalculateMinMax();
+        recalculateRegionMap();
+        recalculatePerimeter();
+        recalculateSides();
+    }
+
+    void recalculateMinMax()
+    {
         // calculate min max x y
         _minX = 100000;
         _maxX = 0;
         _minY = 100000;
         _maxY = 0;
-        for(int i=nodes.size()-1; i>=0; --i)
+        for(int i=_nodes.size()-1; i>=0; --i)
         {
-            if( nodes[i].first < _minX )
+            if( _nodes[i].first < _minX )
             {
-                _minX = nodes[i].first;
+                _minX = _nodes[i].first;
             }
-            if( nodes[i].first > _maxX )
+            if( _nodes[i].first > _maxX )
             {
-                _maxX = nodes[i].first;
+                _maxX = _nodes[i].first;
             }
-            if( nodes[i].second < _minY )
+            if( _nodes[i].second < _minY )
             {
-                _minY = nodes[i].second;
+                _minY = _nodes[i].second;
             }
-            if( nodes[i].second > _maxY )
+            if( _nodes[i].second > _maxY )
             {
-                _maxY = nodes[i].second;
+                _maxY = _nodes[i].second;
             }
         }
         // max values are included, so size+1
         _maxX++;
         _maxY++;
+    }
 
+    void recalculateRegionMap()
+    {
         // generate minified input
         _regionMap = std::vector< std::vector<bool> >(_maxY-_minY, std::vector<bool>(_maxX-_minX, false));
-        for(int i=nodes.size()-1; i>=0; --i)
+        for(int i=_nodes.size()-1; i>=0; --i)
         {
-            int x = nodes[i].first - _minX;
-            int y = nodes[i].second - _minY;
+            int x = _nodes[i].first - _minX;
+            int y = _nodes[i].second - _minY;
             _regionMap[y][x] = true;
         }
+    }
 
+    void recalculatePerimeter()
+    {
         _perimeter = 0;
+
         // find horizontal sides for perimeter
         for(int y=0; y<_regionMap.size(); ++y)
         {
@@ -69,7 +86,8 @@ public:
             // look for region changes in file
             for(int x=0; x<_regionMap[y].size(); ++x)
             {
-                if( (findingStart && _regionMap[y][x] == true) || (!findingStart && _regionMap[y][x] == false) )
+                if( (findingStart && _regionMap[y][x] == true) || 
+                    (!findingStart && _regionMap[y][x] == false) )
                 {
                     _perimeter++;
                     findingStart = !findingStart;
@@ -90,7 +108,8 @@ public:
             // look for region changes in column
             for(int y=0; y<_regionMap.size(); ++y)
             {
-                if( (findingStart && _regionMap[y][x] == true) || (!findingStart && _regionMap[y][x] == false) )
+                if( (findingStart && _regionMap[y][x] == true) || 
+                    (!findingStart && _regionMap[y][x] == false) )
                 {
                     _perimeter++;
                     findingStart = !findingStart;
@@ -104,21 +123,86 @@ public:
             }
         }
     }
+
+    void recalculateSides()
+    {
+        _sides = 0;
+
+        // find horizontally for sides
+        for(int x=0; x<_regionMap[0].size(); ++x)
+        {
+            // look for side end at right of column
+            bool findingStart = true;
+            for(int y=0; y<_regionMap.size(); ++y)
+            {
+                if( findingStart && _regionMap[y][x] && (x+1 >= _regionMap[0].size() || !_regionMap[y][x+1]) )
+                {
+                    findingStart = false;
+                    _sides++;
+                }
+                else if( !findingStart && (!_regionMap[y][x] || (x+1 < _regionMap[0].size() && _regionMap[y][x+1])) )
+                {
+                    findingStart = true;
+                }
+            }
+            findingStart = true;
+            // look for side end at left of column
+            for(int y=0; y<_regionMap.size(); ++y)
+            {
+                if( findingStart && _regionMap[y][x] && (x-1 < 0 || !_regionMap[y][x-1]) )
+                {
+                    findingStart = false;
+                    _sides++;
+                }
+                else if( !findingStart && (!_regionMap[y][x] || (x-1 >= 0 && _regionMap[y][x-1])) )
+                {
+                    findingStart = true;
+                }
+            }
+        }
+
+        // find vertically for sides
+        for(int y=0; y<_regionMap.size(); ++y)
+        {
+            // look for side end at bottom of row
+            bool findingStart = true;
+            for(int x=0; x<_regionMap[0].size(); ++x)
+            {
+                if( findingStart && _regionMap[y][x] && (y+1 >= _regionMap.size() || !_regionMap[y+1][x]) )
+                {
+                    findingStart = false;
+                    _sides++;
+                }
+                else if(!findingStart && (!_regionMap[y][x] || (y+1 < _regionMap.size() && _regionMap[y+1][x])) )
+                {
+                    findingStart = true;
+                }
+            }
+            findingStart = true;
+            // look for side end at top of row
+            for(int x=0; x<_regionMap[0].size(); ++x)
+            {
+                if( findingStart && _regionMap[y][x] && (y-1 < 0 || !_regionMap[y-1][x]) )
+                {
+                    findingStart = false;
+                    _sides++;
+                }
+                else if( !findingStart && (!_regionMap[y][x] || (y-1 >= 0 && _regionMap[y-1][x])) )
+                {
+                    findingStart = true;
+                }
+            }
+        }
+    }
 };
 
 class Helper : public IAoCHelper
 {
 public:
-    std::map<char, std::vector<int>> _regionsArea;
-    std::map<char, int> _regionsPerimeter;
-
     std::map<char, std::vector<Region>> _plantsRegions;
 
     void prepareInput()
     {
-//        prepareAreas();
-//        preparePerimeter();
-
         // prepare regions
         for(int y=0; y<_fileInput.size(); ++y)
         {
@@ -183,98 +267,7 @@ public:
         }
         return nodes;
     }
-/*
-    void prepareAreas()
-    {
-        for(const auto& [region, area] : _regionsArea)
-        {
-            findRegion(region, currentInput);
-        }
-    }
 
-    void prepareAreas2()
-    {
-        // count regions
-        for(int y=0; y<_fileInput.size(); ++y)
-        {
-            for(int x=0; x<_fileInput[y].size(); ++x)
-            {
-                _regionsArea[ _fileInput[y][x] ]++;
-            }
-        }
-
-//        std::cout << "area " << _regionsArea['B'] << std::endl;
-    }
-
-    void findRegion(char region)
-    {
-        std::vector<std::string> currentInput = _fileInput;
-
-        for(int y=0; y<currentInput.size(); ++y)
-        {
-            for(int x=0; x<currentInput[y].size(); ++x)
-            {
-                if( _fileInput[y][x] == region )
-                {
-                    findRegion(region, currentInput, x, y);
-                }
-            }
-        }
-    }
-
-    void preparePerimeters()
-    {
-    }
-
-    void preparePerimeters2()
-    {
-        // find sides for perimeters
-        for(const auto& [region, area] : _regionsArea)
-        {
-            // find horizontal sides
-            for(int y=0; y<_fileInput.size(); ++y)
-            {
-                bool findingStart = true;
-                // look for region changes in file
-                for(int x=0; x<_fileInput[y].size(); ++x)
-                {
-                    if( (findingStart && _fileInput[y][x] == region) || (!findingStart && _fileInput[y][x] != region) )
-                    {
-                        _regionsPerimeter[region]++;
-                        findingStart = !findingStart;
-                    }
-                }
-                // handle side at end of input
-                if( !findingStart )
-                {
-                    _regionsPerimeter[region]++;
-                    findingStart = !findingStart;
-                }
-            }
-
-            // find vertical sides
-            for(int x=0; x<_fileInput[0].size(); ++x)
-            {
-                bool findingStart = true;
-                // look for region changes in column
-                for(int y=0; y<_fileInput.size(); ++y)
-                {
-                    if( (findingStart && _fileInput[y][x] == region) || (!findingStart && _fileInput[y][x] != region) )
-                    {
-                        _regionsPerimeter[region]++;
-                        findingStart = !findingStart;
-                    }
-                }
-                // handle side at end of input
-                if( !findingStart )
-                {
-                    _regionsPerimeter[region]++;
-                    findingStart = !findingStart;
-                }
-            }
-        }
-    }
-*/
     virtual void calculateFirstPuzzleAnswer()
     {
         this->_firstPuzzleAnswer = 0;
@@ -286,7 +279,7 @@ public:
             {
                 Region region = regions[i];
                 this->_firstPuzzleAnswer += region.getPrice();
-                std::cout << plant << " " << i << ": " << region.getArea() << " x " << region.getPerimeter() << " $" << region.getPrice() << std::endl;
+//                std::cout << plant << " " << i << ": " << region.getArea() << " x " << region.getPerimeter() << " $" << region.getPrice() << std::endl;
             }
         }
     }
@@ -295,7 +288,16 @@ public:
     {
         this->_secondPuzzleAnswer = 0;
 
-
+        // calculate total price
+        for(const auto& [plant, regions] : _plantsRegions)
+        {
+            for(int i=0; i<regions.size(); ++i)
+            {
+                Region region = regions[i];
+                this->_secondPuzzleAnswer += region.getPriceWithDiscount();
+//                std::cout << plant << " " << i << ": " << region.getArea() << " x " << region.getSides() << " $" << region.getPriceWithDiscount() << std::endl;
+            }
+        }
     }
 };
 
@@ -304,7 +306,6 @@ int main()
     std::cout << "Advent of Code 2024 - Day 12" << std::endl;
 
     const std::string inputFileName = "input";
-//    const std::string inputFileName = "debuginput";
     Helper helper;
     helper.calculateAnswers(inputFileName);
 
